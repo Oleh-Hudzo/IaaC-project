504328db47983f9090e57ca473abbcbfb7e89c2a
Oleh-Hudzo <myhudzio@gmail.com>
2023-01-26 12:15:11 +0200

    added subnet as a module

--- main.tf
+++ main.tf
@@ -12,15 +12,6 @@ provider "aws" {
   region = var.region
 }
 
-variable "region" {}
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
-variable "my_ip" {}
-variable "instance_type" {}
-variable "public_ssh_key_location" {}
-
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
     tags = {
@@ -28,36 +19,12 @@ resource "aws_vpc" "myapp-vpc" {
     }
 }
 
-resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.subnet_cidr_block
-    availability_zone = var.avail_zone
-    tags = {
-        "Name" = "${var.env_prefix}-subnet-1"
-    }
-}
-
-resource "aws_route_table" "myapproute-table" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.myapp-internet-gateway.id
-    }
-    tags = {
-      "Name" = "${var.env_prefix}-rout-table"
-    }
-}
-
-resource "aws_internet_gateway" "myapp-internet-gateway" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    tags = {
-      "Name" = "${var.env_prefix}-internet-gateway"
-    }
-}
-
-resource "aws_route_table_association" "a-rtb-subnet" {
-    subnet_id = aws_subnet.myapp-subnet-1.id
-    route_table_id = aws_route_table.myapproute-table.id
+module "myapp-subnet" {
+  source = "./modules/subnet"
+  subnet_cidr_block = var.subnet_cidr_block
+  avail_zone = var.avail_zone
+  env_prefix = var.env_prefix
+  vpc_id = aws_vpc.myapp-vpc.id
 }
 
 resource "aws_security_group" "myapp-sg" {
@@ -105,14 +72,6 @@ data "aws_ami" "latest-amazon-linux-image" {
     }
 }
 
-output "aws-ami-id" {
-    value = data.aws_ami.latest-amazon-linux-image.id
-}
-
-output "ec2_public_ip" {
-    value = aws_instance.myapp-server.public_ip
-}
-
 resource "aws_key_pair" "ssh-key" {
     key_name = "server-key"
     public_key = file(var.public_ssh_key_location)
@@ -122,7 +81,7 @@ resource "aws_instance" "myapp-server" {
     ami = data.aws_ami.latest-amazon-linux-image.id
     instance_type = var.instance_type
     
-    subnet_id = aws_subnet.myapp-subnet-1.id
+    subnet_id = module.myapp-subnet.subnet.id
     vpc_security_group_ids = [aws_security_group.myapp-sg.id]
     availability_zone = var.avail_zone
     
