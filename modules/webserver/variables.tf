7c3b7eafbcb23d6c014a624d03adcd34d83ea4d5
Oleh-Hudzo <myhudzio@gmail.com>
2023-01-26 14:26:35 +0200

    added webserver as a module

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,7 @@
+variable "vpc_id" {}
+variable "my_ip" {}
+variable "public_ssh_key_location" {}
+variable "avail_zone" {}
+variable "instance_type" {}
+variable "env_prefix" {}
+variable "subnet_id" {}
